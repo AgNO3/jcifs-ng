1c42efd18b1acec59944811f924114839f55d9f6
Moritz Bechler <bechler@agno3.eu>
2016-08-09 10:51:20 +0200

    Add test case for exclusive locking.

--- src/test/java/jcifs/tests/ConcurrencyTest.java
+++ src/test/java/jcifs/tests/ConcurrencyTest.java
@@ -23,6 +23,8 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -37,6 +39,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import jcifs.smb.NtStatus;
+import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
 
 
@@ -71,6 +75,145 @@ public class ConcurrencyTest extends BaseCIFSTest {
         this.executor.shutdown();
         this.executor.awaitTermination(10, TimeUnit.SECONDS);
         super.tearDown();
+    }
+
+
+    @Test
+    public void testExclusiveLock () throws InterruptedException, MalformedURLException, UnknownHostException {
+
+        String fname = makeRandomName();
+
+        ExclusiveLockFirst f = new ExclusiveLockFirst(new SmbFile(getDefaultShareRoot(), fname, SmbFile.FILE_NO_SHARE));
+        ExclusiveLockSecond s = new ExclusiveLockSecond(f, new SmbFile(getDefaultShareRoot(), fname, SmbFile.FILE_NO_SHARE));
+
+        List<MultiTestCase> runnables = new ArrayList<>();
+        runnables.add(f);
+        runnables.add(s);
+        runMultiTestCase(runnables, 5);
+    }
+
+    private class ExclusiveLockFirst extends MultiTestCase {
+
+        private Object startedLock = new Object();
+        private volatile boolean started;
+
+        private Object shutdownLock = new Object();
+        private volatile boolean shutdown;
+        private SmbFile file;
+
+
+        /**
+         * @param smbFile
+         * 
+         */
+        public ExclusiveLockFirst ( SmbFile smbFile ) {
+            this.file = smbFile;
+        }
+
+
+        public void waitForStart () throws InterruptedException {
+            synchronized ( this.startedLock ) {
+                while ( !this.started ) {
+                    this.startedLock.wait();
+                }
+            }
+        }
+
+
+        public void shutdown () {
+            this.shutdown = true;
+            synchronized ( this.shutdownLock ) {
+                this.shutdownLock.notify();
+            }
+        }
+
+
+        /**
+         * {@inheritDoc}
+         *
+         * @see java.lang.Runnable#run()
+         */
+        @Override
+        public void run () {
+
+            try {
+                SmbFile f = this.file;
+                f.createNewFile();
+                try {
+                    try ( OutputStream os = f.getOutputStream() ) {
+                        log.error("Open1");
+                        synchronized ( this.startedLock ) {
+                            this.started = true;
+                            this.startedLock.notify();
+                        }
+                        while ( !this.shutdown ) {
+                            synchronized ( this.shutdownLock ) {
+                                this.shutdownLock.wait();
+                            }
+                        }
+                    }
+                    catch ( InterruptedException e ) {}
+                    log.error("Closed1");
+                }
+                finally {
+                    f.delete();
+                }
+                this.completed = true;
+            }
+            catch ( IOException e ) {
+                log.error("Test case failed", e);
+            }
+        }
+
+    }
+
+    private class ExclusiveLockSecond extends MultiTestCase {
+
+        private ExclusiveLockFirst first;
+        private SmbFile file;
+
+
+        /**
+         * @param f
+         * @param smbFile
+         */
+        public ExclusiveLockSecond ( ExclusiveLockFirst f, SmbFile smbFile ) {
+            this.first = f;
+            this.file = smbFile;
+        }
+
+
+        /**
+         * {@inheritDoc}
+         *
+         * @see java.lang.Runnable#run()
+         */
+        @Override
+        public void run () {
+            try {
+                SmbFile f = this.file;
+                this.first.waitForStart();
+                try ( OutputStream os = f.getOutputStream() ) {
+                    log.error("Open2");
+                }
+                catch ( IOException e ) {
+                    if ( e instanceof SmbException && ( (SmbException) e ).getNtStatus() == NtStatus.NT_STATUS_SHARING_VIOLATION ) {
+                        this.completed = true;
+                        return;
+                    }
+                    throw e;
+                }
+                finally {
+                    this.first.shutdown();
+                }
+            }
+            catch (
+                IOException |
+                InterruptedException e ) {
+                log.error("Test case failed", e);
+            }
+        }
+
     }
 
 
