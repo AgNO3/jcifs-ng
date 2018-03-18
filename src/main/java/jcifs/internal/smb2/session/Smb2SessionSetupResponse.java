3cebaf56ef149ffba6c51b7b1ab04ce1114d1b39
guybmayor <guymayor@gmail.com>
2018-03-18 13:58:23 +0200

    return whether the session setup response flag has been signed.

--- src/main/java/jcifs/internal/smb2/session/Smb2SessionSetupResponse.java
+++ src/main/java/jcifs/internal/smb2/session/Smb2SessionSetupResponse.java
@@ -150,5 +150,11 @@ public class Smb2SessionSetupResponse extends ServerMessageBlock2Response {
     public byte[] getBlob () {
         return this.blob;
     }
-
+    
+     /**
+     * @return whether the packet has been signed.
+     */
+    public boolean isSessionSigned() {
+        return ( this.sessionFlags & SMB2_FLAGS_SIGNED ) != 0;
+    }
 }
